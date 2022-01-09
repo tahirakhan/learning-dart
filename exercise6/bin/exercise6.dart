b87d6690e6f6b470e7d55766374cc2d610367852
Tahir Ali Khan <sigmaprofile@hotmail.com>
2022-01-09 15:48:46 +0500

    Update exercise6.dart

--- exercise6/bin/exercise6.dart
+++ exercise6/bin/exercise6.dart
@@ -1,3 +1,17 @@
+import 'dart:mirrors';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  int myAge = 40;
+  int yourAge = 300;
+
+  String message = "";
+  if (myAge > yourAge) {
+    message = "My age is greater then your age.";
+  } else if (myAge == yourAge) {
+    message = "We are equal.";
+  } else if (myAge < yourAge) {
+    message = "My age is less then your age.";
+  }
+
+  print(message);
 }
